74f6d47d19d0cbdef8de4c71726bff20a0653d9b
Julitana <aangulo@vlacademy.com>
2025-02-03 20:13:10 -0600

    Se hicieron agregaron las funciones

--- JS/PagesJS/Practica_Calculadora.js
+++ JS/PagesJS/Practica_Calculadora.js
@@ -5,5 +5,19 @@ console.log(`Funcionando Archivo: Practica_Calculadora`);
     let nombre = document.getElementById("nombre").value;
     let edad = document.getElementById("edad").value;
 
+     //Mostrar funcion 
+     debugger
+    console.log('Mostrar Nombre:',nombre);
+    console.log('Mostrar Edad:',edad);
+    alert (`nombre ${nombre} edad ${edad}`)
+}
+
+function realizarSuma() {
+    console.log. ("Funcionado realizarSuma")
+    let numero1 = document.getElementById('numero1')value;
+    let numero2 = document.getElementById('numero2')value;
+
+    console.log ("numero1")
+    console.log ("numero2")
 }
 
